7a6698439f4da31f90bfd5de9d3d4876cf6e541a
Antony Nicolas Huaman Alikhan <73962071@continental.edu.pe>
2025-12-22 21:31:11 -0500

    Initial Commit

---
+++ UberCloneSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UberCloneSwiftUI
+//
+//  Created by Antony Huaman Alikhan on 22/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UberCloneSwiftUI/UberCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneSwiftUIApp.swift
+//  UberCloneSwiftUI
+//
+//  Created by Antony Huaman Alikhan on 22/12/25.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
